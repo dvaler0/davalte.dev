97e3453dc35f2e2b8a16db7c2b796ca5d997188a
David Valero <valeroterrondavid@gmail.com>
2023-03-18 16:41:25 +0100

    Add TailwindCSSto the project and style some components

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,28 @@ function App () {
   return (
     <>
 
-      <main>
+      <main className='block w-full max-w-6xl m-auto'>
         <section>
           <h1>Hey👋 I'm David Valero</h1>
         </section>
 
         <section>
-          <h2>Experience</h2>
-          {
-            experience.map(({ company, period, text }) => {
-              return (
-                <ExperienceCard
-                  key={company}
-                  company={company}
-                  period={period}
-                  text={text}
-                />
-              )
-            })
-          }
+          <h2 className='font-bold text-2xl mb-7'>Experience</h2>
+          <section className='flex flex-col sm:flex-row gap-7 '>
+            {
+              experience.map(({ company, period, text }) => {
+                return (
+                  <ExperienceCard
+                    key={company}
+                    company={company}
+                    period={period}
+                    text={text}
+                  />
+                )
+              })
+            }
+          </section>
+
         </section>
 
       </main>
--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -1,7 +1,7 @@
-export function ExperienceCard ({ children, company, period, text }) {
+export function ExperienceCard ({ company, period, text }) {
   return (
-    <article>
-      <h2>{company}</h2>
+    <article className='flex flex-col p-12 bg-white text-black rounded-lg border border-gray-200 shadow-md shadow-gray-100'>
+      <h2 className='font-bold text-2xl'>{company}</h2>
       <span>{period}</span>
       <p>{text}</p>
     </article>
